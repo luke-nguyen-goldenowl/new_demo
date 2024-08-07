497b2ca54ab52641de93bf01ab8abcd86b6268bb
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2024-08-07 14:25:22 +0700

    feat 20

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // Commit 20
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
